59b30cda793d08119c585298c9525a8fdf75c1b0
Alvaro Rincon <alvarorinconc1@gmail.com>
2025-04-26 21:45:53 -0500

    Modelo de datos.
    - Se crea el object Contants y se agrega la URL base de la API "The Rick and Morty API".
    - Se crean las clases "Info", "ListaPersonajes", "Location", "Origin" y "Personaje" para administrar los datos devueltos por la API.

---
+++ app/src/main/java/com/example/rickandmorty/model/ListaPersonajes.kt
@@ -0,0 +1,6 @@
+package com.example.rickandmorty.model
+
+data class ListaPersonajes(
+    val info: Info,
+    val results: List<Personaje>
+)
---
+++ app/src/main/java/com/example/rickandmorty/utils/Contants.kt
@@ -0,0 +1,5 @@
+package com.example.rickandmorty.utils
+
+object Contants {
+    const val BASE_URL = "https://rickandmortyapi.com/api/"
+}
